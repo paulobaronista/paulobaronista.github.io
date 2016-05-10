799d2427b2fdd00fcffe12a827f212b2a1fe758a
PauloBaronista <paulobaronista@gmail.com>
2016-05-10 17:36:18 -0300

    Subindo Projeto Bid Travel Brasil - Versão Teste

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+      //menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+   
